801a65bae4e9b81d56e9cc3556f57ebfd4ba18c3
xiaohouzi456 <907277759@qq.com>
2017-09-14 15:39:19 +0800

    返回是否设置过密码

--- lock/src/main/java/com/hou/lock/utils/GestureLock.java
+++ lock/src/main/java/com/hou/lock/utils/GestureLock.java
@@ -1,6 +1,7 @@
 package com.hou.lock.utils;
 
 import android.content.Context;
+import android.text.TextUtils;
 
 /**
  * Created by Zxl on 2017/9/8
@@ -9,15 +10,18 @@ import android.content.Context;
 public class GestureLock {
     /**
      * 通过用户Id来区分用户和单独保存密码
-     *
-     * @param userId
      */
     public static void setAlias(Context context, String userId) {
         Sp.init(context);
         Sp.setAlias(userId);
     }
 
-    public static String getLock() {
-        return Sp.getDefault().getLock();
+    /**
+     * 是否设置了密码
+     *
+     * @return true表示设置过
+     */
+    public static boolean isSetLockPassword() {
+        return !TextUtils.isEmpty(Sp.getDefault().getLock());
     }
 }
